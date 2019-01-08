70542ea0b57e0116d11868779390fc8b216309b6
Greg Morgan <gregm9999@gmail.com>
2019-01-08 21:12:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(feet) {
-  if (feet<400) {
+  if (feet <400) {
     return "This one is on me!"
   }
   if (feet > 2000) {
     return 'I will gladly take your thirty bucks.'
   }
-  if (feet > 2500) {
+  if (feet >= 2500) {
     return 'No can do.'
   }
 }
